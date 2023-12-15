2452683d1899ef40209bc84cee2b597eed0b5c1f
shivamengg102 <142868775+shivamengg102@users.noreply.github.com>
2023-12-15 09:37:40 +0530

    Add files via upload

---
+++ diamond.java
@@ -0,0 +1,28 @@
+public class advance5 {
+    public static void main(String[] args) {
+        int n = 4;
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            // space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            // space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
